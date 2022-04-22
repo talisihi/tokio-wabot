e2749f078d13a3df62ca4efd055e846697374c28
talisihi <80786632+talisihi@users.noreply.github.com>
2022-04-23 00:40:38 +0400

    Update and rename plugins/addprem.js to plaginlər/Premiumver.js

--- plaginlər/Premiumver.js
+++ plaginlər/Premiumver.js
@@ -3,10 +3,10 @@ let handler = async (m, { conn, text }) => {
     let who
     if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : text
     else who = m.chat
-    if (!who) throw `tag the person!`
+    if (!who) throw `Tag edin !`
     if (global.prems.includes(who.split`@`[0])) throw 'he is premium!'
     global.prems.push(`${who.split`@`[0]}`)
-    conn.reply(m.chat, `@${who.split`@`[0]} now premium!`, m, {
+    conn.reply(m.chat, `@${who.split`@`[0]} premium verildi !`, m, {
         contextInfo: {
             mentionedJid: [who]
         }

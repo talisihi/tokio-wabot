f750804be16d62aefe1d5680dca80770c2e8858b
talisihi <80786632+talisihi@users.noreply.github.com>
2022-05-01 09:57:33 +0400

    Update broadcastgroups.js

--- plugins/broadcastgroups.js
+++ plugins/broadcastgroups.js
@@ -3,8 +3,8 @@ let handler  = async (m, { conn, text }) => {
   let cc = text ? m : m.quoted ? await m.getQuotedObj() : false || m
   let teks = text ? text : cc.text
   conn.reply(m.chat, `_Sent the broadcast message to ${groups.length} groups_`, m)
-  for (let id of groups) await conn.copyNForward(id, conn.cMod(m.chat, cc, /bc|broadcast/i.test(teks) ? teks : teks + '\n' + readMore + '\n' + '「 All Group Broadcast 」'), true).catch(_=>_)
-  m.reply('Broadcast done for All Groups :)')
+  for (let id of groups) await conn.copyNForward(id, conn.cMod(m.chat, cc, /bc|broadcast/i.test(teks) ? teks : teks + '\n' + readMore + '\n' + '「 All BC Çatlar 」'), true).catch(_=>_)
+  m.reply('Mesajınız qruplara göndərildi :)')
 }
 handler.help = ['broadcastgroup/bcgc'].map(v => v + ' <teks>')
 handler.tags = ['owner']

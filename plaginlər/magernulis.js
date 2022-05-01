ffb75201b0358f62c04ae204f75f6a99d0526d53
talisihi <80786632+talisihi@users.noreply.github.com>
2022-05-01 12:27:49 +0400

    Update and rename plugins/magernulis.js to plaginlər/magernulis.js

--- plaginlər/magernulis.js
+++ plaginlər/magernulis.js
@@ -1,13 +1,13 @@
 // Magernulis By MFarelS:V
-// Code by DrawlNag
-// Recode by Nurutomo :V
+// Code by Rufo
+// Recode by Rufo :V
 let handler  = async (m, { command, conn, text }) => {
   let id = (command.match(/[1-6]$/) || [])[0] || ''
   await conn.sendFile(m.chat, global.API('xteam', '/magernulis' + id, {
     text,
     nama: conn.getName(m.sender),
     kelas: ' '
-  }, 'APIKEY'), 'nulis.jpg', 'Nahh it\'s done...', m)
+  }, 'APIKEY'), 'nulis.jpg', 'Nahh bitdi\'s done...', m)
 }
 handler.help = new Array(6).fill('lazywrite').map((v, i) => v + (i + 1) + ' <teks>')
 handler.tags = ['nulis']

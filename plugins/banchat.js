7f78fd9377133f3639da6aa0d533358548423e29
talisihi <80786632+talisihi@users.noreply.github.com>
2022-05-01 09:39:30 +0400

    Update banchat.js

--- plugins/banchat.js
+++ plugins/banchat.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn, participants }) => {
   // if (participants.map(v=>v.jid).includes(global.conn.user.jid)) {
     global.db.data.chats[m.chat].isBanned = true
-    m.reply('Done!')
+    m.reply('hazır!')
   // } else m.reply('There is a host number here...')
 }
 handler.help = ['banchat']
